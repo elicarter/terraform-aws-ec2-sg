0228a961ab9f3414df39cfb223df8a982c4cb070
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 08:59:53 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
-variable "existing_vpc_id" {
-    description = "Existing VPC ID"
-  default = "vpc-0532e092970c8807e"
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = var.existing_vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
